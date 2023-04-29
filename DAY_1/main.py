caf39381bd79f344d0dd98c1eb5e1ac8648f5ac3
SoumyajyotiSadhukhan <97792264+SoumyajyotiSadhukhan@users.noreply.github.com>
2023-04-29 17:09:13 +0530

    Add files via upload

---
+++ DAY_1/main.py
@@ -0,0 +1,10 @@
+# 1. Creating a greeting
+print("Welcome to Band name generator for your pets")
+# 2. Ask the user for the city the grew up in.
+a = input("Enter the name of the city in which you grew up? \n")
+# 3. Ask the user for the name of the pet
+b = input("Enter the name of your pet \n")
+# 4. Combine the name of their city and pet to generate the band name
+print("The band name is: ", a,b)
+# 5. Create a greeting
+print("Thank you")
